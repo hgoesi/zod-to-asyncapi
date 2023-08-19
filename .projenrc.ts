87cd729c523ba0ec364e39b3ee342b485782a95b
Johannes Konings <johannes.konings@outlook.com>
2023-08-19 18:29:27 +0200

    init

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,15 @@
-import { typescript } from 'projen';
+import { typescript, javascript } from 'projen';
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'zod-to-asyncapi',
   projenrcTs: true,
+  packageManager: javascript.NodePackageManager.NPM,
+  releaseToNpm: true,
+
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: '@hgoesi/zod-to-asyncapi',
 });
 project.synth();
